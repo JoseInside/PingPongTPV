3d96c1d6174b137ce0e9c6fca5da43290a571926
JoseInside <josemonr@ucm.es>
2017-06-11 11:18:22 +0200

    Final Release

--- P5_PingPong/main.cpp
+++ P5_PingPong/main.cpp
@@ -1,8 +1,5 @@
-#include "PingPong.h"
-
-
 /*
-			///////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 
 
 	PRÁCTICA 6  -  PING PONG (TEORÍA PROGRAMACIÓN DE VIDEOJUEGOS)
@@ -10,11 +7,18 @@
 			AUTOR: JOSE MARÍA MONREAL GONZÁLEZ
 			
 
-			///////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 */
+
+
+#include "PingPong.h"
+
 using namespace std;
 
 int main(int ac, char** av) {
+
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Leaks
+
 	PingPong g;
 	g.start();
 	return 0;
